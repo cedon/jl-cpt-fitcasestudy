2170f5eb9200b83a5c1af48e20e172d9cc92685c
Jon Liebold <jliebold@outlook.com>
2017-05-22 08:21:29 -0400

    Initial Commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# JL CPT Fitness Case Study
+
+[![License](https://poser.pugx.org/automattic/jetpack/license.svg)](http://www.gnu.org/licenses/gpl-2.0.html)
+
+JL CPT Fitness Case Study is a WordPress plugin the creates a Case Study custom post type to help Fitness professionals. 
---
+++ jl-cpt-fitcasestudy.php
@@ -0,0 +1,23 @@
+<?php
+
+/*
+Plugin Name: JL Fitness Case Study Custom Post Type
+Plugin URI: http://jonliebold.com
+Description: A plugin to create a custom post type for Case Studies.
+Version: 1.0
+Author: Jon Liebold
+Author URI: http://jonliebold.com
+License: GPLv2 or later
+*/
+
+// Options Page
+
+// Custom Post Type Definition
+
+// Custom Taxonomy Definition
+
+// JSON Linked Data
+
+// Post Template
+
+?>
