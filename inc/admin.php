1a13a8c1a31f5d71ab2b0b74fcf93fb84d0dec2a
Jon Liebold <jliebold@outlook.com>
2017-05-26 12:17:53 -0400

    Created initial test action for saving custom post type

--- inc/admin.php
+++ inc/admin.php
@@ -5,13 +5,13 @@
  */
 
 /**
- * Creates the Admin Panel Subpage for Options and places it in the submenu defined by the custom
+ * Creates the Admin Panel Subpage for Plugin Options and places it in the submenu defined by the custom
  * post type delcaration.
  */
 
 function fitcase_admin_menu() {
 	add_submenu_page(
-		'edit.php?post_type=fit-case-study',
+		'edit.php?post_type=fitcasestudy',
 		__( 'Options', 'fitcasestudy' ),
 		__( 'Options', 'fitcasestudy' ),
 		'administrator',
@@ -101,13 +101,116 @@ function add_meta_boxes_fitcasestudy() {
         'normal',
         'high'
     );
+
+	add_meta_box(
+		'fitcase-client-results',
+		__( 'Client Results', 'fitcasestudy' ),
+		'meta_box_clientresults_callback',
+		'fitcasestudy',
+		'normal',
+		'high'
+	);
+
+	add_meta_box(
+	        'fitcase-study-client-pics',
+            __( 'Client Progress Photos', 'fitcasestudy' ),
+            'meta_box_clientpics_callback',
+            'fitcasestudy',
+            'side',
+            'default'
+    );
 }
 add_action( 'add_meta_boxes', 'add_meta_boxes_fitcasestudy' );
 
-function meta_box_clientinfo_callback() {
-    echo '<h2>Client Information</h2>';
+function meta_box_clientinfo_callback( $post )  {
+    global $fitcase_post_meta;
+    $fitcase_post_meta = get_post_meta( $post->ID );
+	wp_nonce_field( plugin_basename( __FILE__ ), 'fitcase-nonce' );
+
+	echo '<pre>';
+	print_r($fitcase_post_meta);
+	echo '</pre>';
+?>
+
+    <p>Please enter in the information on the client whom you wish to feature in a case story.</p>
+    <p>
+        <label for="fitcase-client-name"><?php _e( 'Client Name', 'fitcasestudy' ); ?></label>
+        <input type="text" name="fitcase_client_name" id="fitcase_client_name" size="25" value="<?php fitcase_field_value( 'client_name' ); ?>" />
+    </p>
+
+    <p>
+        <label for="fitcase-client-age"><?php _e( 'Client Age', 'fitcasestudy' ); ?></label>
+        <input type="text" name="fitcase_client-age" id="fitcase_client_age" value="<?php fitcase_field_value( 'fitcase-client-age' ); ?>" size="3" maxlength="3" />
+    </p>
+
+    <p>
+        <label for="fitcase-client-gender"><?php _e( 'Client Gender', 'fitcasestudy' ); ?></label>
+        <select name="fitcase-client-gender">
+            <option value="male">Male</option>
+            <option value="female">Female</option>
+            <option value="Other">Other</option>
+        </select>
+    </p>
+
+<?php
 }
 
-function meta_box_clienthist_callback() {
-	echo '<h2>Client History</h2>';
+function meta_box_clienthist_callback( $post ) {
+	echo '<pre>';
+	//print_r($post_id);
+	echo '</pre>';
 }
+
+function meta_box_clientresults_callback( $post ) {
+	echo '<h2>Client Results</h2>';
+}
+
+function meta_box_clientpics_callback( $post ) {
+    $fitcase_post_meta = get_post_meta( $post->ID );
+?>
+
+    <p>Before/After photos of client</p>
+
+<?php
+}
+
+/**
+ * Save Meta Box Data to the database
+ */
+function fitcase_save_meta( $post_id, $post, $update ) {
+
+    global $fitcase_post_meta;
+    $fitcase_post_meta = get_post_meta( $post_id );
+	error_log( 'fitcase_save_meta is firing');
+
+
+	if ( defined( 'DOING_AUTOSAVE' ) && DOING_AUTOSAVE ) {
+	    error_log( 'fitcase_save_meta: Doing Autosave');
+        return;
+    }
+
+    // Verify Nonce
+    
+
+    if ( !current_user_can( 'edit_post', $post_id ) ) {
+        error_log('fitcase_save_meta: user cannot edit posts');
+        return;
+    }
+
+    if ( !current_user_can( 'edit_page' , $post_id ) ) {
+            error_log( 'fitcase_save_meta: user cannot edit pages');
+            return;
+    }
+
+    error_log('All Conditions are Met');
+
+    $fitcase_client_name = $_POST['fitcase_client_name'];
+    update_post_meta( $post_id, 'client_name', $fitcase_client_name );
+
+    error_log( 'fitcase_save_meta fired');
+
+    error_log('$_POST Contents:');
+    error_log( print_r($_POST, true));
+
+}
+add_action( 'save_post', 'fitcase_save_meta', 10, 3 );
--- inc/functions.php
+++ inc/functions.php
@@ -6,5 +6,12 @@ function fitcase_field_value( $field_id ) {
 	if ( isset ( $fitcase_post_meta[$field_id] ) ) {
 		echo $fitcase_post_meta[$field_id][0];
 	}
-
 }
+
+function test_save_hook( $post_id, $post, $update ) {
+	$updated = ( $update ) ? "updated" : "saved";
+	//error_log( "Post type of " . $post->post_type . " was " . $updated );
+
+	error_log( print_r($_REQUEST) );
+}
+add_action( 'save_post', 'test_save_hook', 10, 3 );
