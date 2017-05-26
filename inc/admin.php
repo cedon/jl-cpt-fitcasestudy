66713c3a3f4e9f3ef9bb2784dfce0f9d8949abce
Jon Liebold <jliebold@outlook.com>
2017-05-26 17:17:57 -0400

    Added WordPress editor to client history meta box

--- inc/admin.php
+++ inc/admin.php
@@ -134,18 +134,18 @@ function meta_box_clientinfo_callback( $post )  {
 
     <p>Please enter in the information on the client whom you wish to feature in a case story.</p>
     <p>
-        <label for="fitcase-client-name"><?php _e( 'Client Name', 'fitcasestudy' ); ?></label>
-        <input type="text" name="fitcase_client_name" id="fitcase_client_name" size="25" value="<?php fitcase_field_value( 'client_name' ); ?>" />
+        <label for="fitcase_client_name"><?php _e( 'Client Name', 'fitcasestudy' ); ?></label>
+        <input type="text" name="fitcase_client_name" id="fitcase_client_name" size="25" value="<?php fitcase_field_value( 'fitcase_client_name' ); ?>" />
     </p>
 
     <p>
         <label for="fitcase-client-age"><?php _e( 'Client Age', 'fitcasestudy' ); ?></label>
-        <input type="text" name="fitcase_client-age" id="fitcase_client_age" value="<?php fitcase_field_value( 'fitcase-client-age' ); ?>" size="3" maxlength="3" />
+        <input type="text" name="fitcase_client_age" id="fitcase_client_age" value="<?php fitcase_field_value( 'fitcase-client-age' ); ?>" size="3" maxlength="3" />
     </p>
 
     <p>
-        <label for="fitcase-client-gender"><?php _e( 'Client Gender', 'fitcasestudy' ); ?></label>
-        <select name="fitcase-client-gender">
+        <label for="fitcase_client_gender"><?php _e( 'Client Gender', 'fitcasestudy' ); ?></label>
+        <select id="fitcase_client_gender" name="fitcase-client-gender">
             <option value="male">Male</option>
             <option value="female">Female</option>
             <option value="Other">Other</option>
@@ -156,9 +156,22 @@ function meta_box_clientinfo_callback( $post )  {
 }
 
 function meta_box_clienthist_callback( $post ) {
-	echo '<pre>';
-	//print_r($post_id);
-	echo '</pre>';
+    global $fitcase_post_meta;
+
+    $fitcasestudy_client_history_content = $fitcase_post_meta['fitcase_client_history'][0];
+    $editor_id = 'fitcase_client_history';
+
+    if ( $fitcasestudy_client_history_content != '' ) {
+        $editor_content = $fitcasestudy_client_history_content;
+    } else {
+        $editor_content = '';
+    }
+    ?>
+    <p>Enter in the history of the client.</p>
+    <label for="fitcase_client_history" class="screen-reader-text"><?php _e( 'Client History Text Area', 'fitcasestudy' ); ?></label>
+
+    <?php
+    wp_editor( $editor_content, $editor_id );
 }
 
 function meta_box_clientresults_callback( $post ) {
@@ -205,12 +218,14 @@ function fitcase_save_meta( $post_id, $post, $update ) {
     error_log('All Conditions are Met');
 
     $fitcase_client_name = $_POST['fitcase_client_name'];
-    update_post_meta( $post_id, 'client_name', $fitcase_client_name );
+    update_post_meta( $post_id, 'fitcase_client_name', $fitcase_client_name );
+
+    $fitcase_client_history = $_POST['fitcase_client_history'];
+    update_post_meta( $post_id, 'fitcase_client_history', $fitcase_client_history );
 
     error_log( 'fitcase_save_meta fired');
 
-    error_log('$_POST Contents:');
-    error_log( print_r($_POST, true));
+    error_log('$_POST Contents:' . print_r($_POST, true) );
 
 }
 add_action( 'save_post', 'fitcase_save_meta', 10, 3 );
