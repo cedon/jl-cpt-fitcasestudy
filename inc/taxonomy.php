9cfa4c8312ca106a4a2280cdc5812c77bf2b4a6f
Jon Liebold <jliebold@outlook.com>
2017-05-29 21:13:18 -0400

    Fixed error causing taxnomy option not to appear in admin menu

--- inc/taxonomy.php
+++ inc/taxonomy.php
@@ -35,7 +35,7 @@ function fitness_goal_taxonomy() {
 		'show_tagcloud'              => false,
 		'show_in_rest'               => true,
 	);
-	register_taxonomy( 'fitnessgoal', array( 'fit-case-study' ), $args );
+	register_taxonomy( 'fitnessgoal', array( 'fitcasestudy' ), $args );
 
 }
 add_action( 'init', 'fitness_goal_taxonomy', 0 );
